b3f7f4c25541990158a1ef4dbacad2c31f49e36b
Chris Constantine <chris@omadahealth.com>
2015-01-16 10:56:18 -0800

    Add attempt to read request level info out of rack environment in WatCatcher::RackMiddleware when the CatcherOfWats didn't populate the rack environment with the rails level request object.

--- lib/wat_catcher/rack_middleware.rb
+++ lib/wat_catcher/rack_middleware.rb
@@ -1,5 +1,20 @@
 module WatCatcher
   class RackMiddleware
+    class WatCatcher::Request
+      attr_accessor :url, :method, :params, :headers, :session
+
+      def initialize(url, method, params,  headers, session)
+        @url = url
+        @method = method
+        @params = params
+        @headers = headers
+        @session = session
+      end
+
+      def filtered_parameters
+        params
+      end
+    end
 
     def initialize(app)
       @app = app
@@ -9,13 +24,33 @@ module WatCatcher
       @app.call(env)
     rescue Exception => e
       if !env["wat_report_disabled"]
-        user, request = nil
-        if env["wat_report"]
-          user = env["wat_report"][:user]
-          request = env["wat_report"][:request]
-        end
+        begin
+          user, request = nil
+          if false && env["wat_report"]
+            user = env["wat_report"][:user]
+            request = env["wat_report"][:request]
+          else
+            rack_env = env
+            rack_request = ::Rack::Request.new(env)
+            params = rack_request.params rescue {}
 
-        WatCatcher::Report.new(e, user: user, request: request)
+            # Build the clean url (hide the port if it is obvious)
+            url = "#{rack_request.scheme}://#{rack_request.host}"
+            url << ":#{rack_request.port}" unless [80, 443].include?(rack_request.port)
+            url << rack_request.fullpath
+
+            headers = {}
+            rack_env.each_pair do |key, value|
+              if key.to_s.start_with?("HTTP_") || ["CONTENT_TYPE", "CONTENT_LENGTH"].include?(key)
+                headers[key.upcase] = value
+              end
+
+            end
+            request = WatCatcher::Request.new(url, rack_request.request_method, params.to_hash, headers, rack_env["rack.session"])
+          end
+        ensure
+          WatCatcher::Report.new(e, user: user, request: request)
+        end
       end
       raise
     end
